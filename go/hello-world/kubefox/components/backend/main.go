6e3dc3823a66c342e46d8400d6e834da9bd5cb38
John Long <xadhatter@pm.me>
2023-10-27 21:34:31 -0400

    blah

--- go/hello-world/kubefox/components/backend/main.go
+++ go/hello-world/kubefox/components/backend/main.go
@@ -28,5 +28,6 @@ func sayWho(k kit.Kontext) error {
 	who := k.EnvDef("who", "World")
 	k.Log().Debugf("The who is '%s'!", who)
 
-	return k.Resp().SendStr(who)
+	// return k.Resp().SendStr(who)
+	return k.Resp().SendBytes("text/plain", file)
 }
